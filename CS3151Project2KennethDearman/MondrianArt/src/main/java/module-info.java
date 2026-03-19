773f319815cbc4a0e6afb9d293d082dc617bdcd7
Kenneth Dearman <164975838+kd00250@users.noreply.github.com>
2026-03-18 21:49:45 -0400

    Delete CS3151Project2KennethDearman/MondrianArt directory
    decided to go off of the professors code

--- CS3151Project2KennethDearman/MondrianArt/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module edu.westga.dsdm.mondrianart {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens edu.westga.dsdm.mondrianart to javafx.fxml;
-    exports edu.westga.dsdm.mondrianart;
-}
